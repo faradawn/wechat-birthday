dc969d84fd45f5f41f18b60aae472d41237e8c93
faradawn <faradawny@gmail.com>
2021-07-07 18:09:53 +0800

    发布v1.0.0

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,61 +4,13 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
-    myArr: {Sheet1: [
-      {'id': '1', "生日":'1999-01-23', 姓名: '哈哈'},
-      {'id': '2', "生日":'2001-01-11', 姓名: '哈哈'},
-      {'id': '3', "生日":'2001-0-11', 姓名: '哈哈'},
-    ]}
+    
   },
-  // end of data
-
-  setLoading(){
-    this.setData({loading: !this.data.loading});
-    console.log('set', this.data.loading)
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+ 
   goForest() {
     wx.navigateTo({
       url: '../dept_forestry/forest'
     })
   },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+
 })
